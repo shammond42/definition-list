cdae372d4af7fe611e1972079b9f827136ca4531
Steven Hammond <shammond@northpub.com>
2024-06-15 10:18:00 -0300

    Removes unused settings code

--- main.ts
+++ main.ts
@@ -1,27 +1,12 @@
-import { EditorState, StateField, Transaction } from '@codemirror/state';
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
 
 export default class DefinitionListPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		console.log('loading definition list plugin');
 		this.registerMarkdownPostProcessor((element, context) => {
       const paragraphs = element.findAll("p");
 
 			for (let paragraph of paragraphs) {
-				console.log(paragraph.innerHTML);
-				
 				const lines = paragraph.innerHTML.split('\n');
 
 				let definitionList;
@@ -32,19 +17,14 @@ export default class DefinitionListPlugin extends Plugin {
 				}
 
 				for(let i=0; i<lines.length; i++) {
-					console.log(lines[0]);
 					const line = lines[i];
 
 					const length = line.endsWith("<br>") ? line.length-4 : line.length;
 					if(line[0] === ':') {
-						// console.log(line);
-						// debugger;
 						const definition = line.substring(1, length).trim();
-						console.log(definition);
 						definitionList.createEl("dd", {text: definition});
 					} else {
 						const term = line.substring(0, length).trim();
-						console.log(term);
 						definitionList.createEl("dt", {text: term});
 					}
 				}
